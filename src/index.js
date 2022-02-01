4dc3cb65c97b238f9c41856fa6f0acc943b0ffa4
大姐姐 <wzxielulu@163.com>
2022-02-01 20:04:01 +0800

    【2.jsx简介】jsx指定自元素

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ import ReactDOM from 'react-dom'
 // 通过使用引号，来将属性值指定为字符串字面量
 // const element = <div tabIndex="0"></div>
 
-const user = {
-  avatarUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%3A%2F%2Fdingyue.ws.126.net%2F2021%2F0306%2F701a4922j00qpjng1001nc000ht00dom.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646308771&t=376abf8646bcad35c9394b6b82c363b4'
-}
+// const user = {
+//   avatarUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%3A%2F%2Fdingyue.ws.126.net%2F2021%2F0306%2F701a4922j00qpjng1001nc000ht00dom.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646308771&t=376abf8646bcad35c9394b6b82c363b4'
+// }
 // 通过使用大括号，来在属性值中插入一个js表达式
-const element = <img src={user.avatarUrl}></img>
+// 如果标签里面没有内容，可以使用 />闭合标签
+// const element = <img src={user.avatarUrl} />
+
+// jsx标签里可以包含很多子元素
+const element = (
+  <div>
+    <h1>Hello</h1>
+    <h2>you see see you</h2>
+  </div>
+)
 ReactDOM.render(
   element,
   document.getElementById('root')
