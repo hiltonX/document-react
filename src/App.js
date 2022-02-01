f2dc2735b850cd92f9afd406ac3da54e4b1abf1f
大姐姐 <wzxielulu@163.com>
2022-02-01 19:30:21 +0800

    cra

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CRA-Learn React
         </a>
       </header>
     </div>
