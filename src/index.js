10fdc960dc6777432d94655d67a73f113d4350fb
大姐姐 <wzxielulu@163.com>
2022-02-01 19:42:52 +0800

    【2.jsx简介】在jsx中嵌入表达式

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const name = '大姐姐'
+const element = <h1>hello, {name}</h1>
+
 ReactDOM.render(
-  <h1>hello,world</h1>,
+  element,
   document.getElementById('root')
 )
 
