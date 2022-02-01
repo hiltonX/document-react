604f4bb4f75a12bb8c868972d9bb2824da5a8f11
大姐姐 <wzxielulu@163.com>
2022-02-01 19:47:47 +0800

    【2.jsx简介】在jsx中嵌入表达式2

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ### Hello World
 如果你在过去几年中并没有使用过 JavaScript，大多数情况下这三点应该能帮到你。
 https://gist.github.com/gaearon/683e676101005de0add59e8bb345340c（打不开）
+
+### JSX简介
+建议将内容包裹在括号中，不是强制要求，但是可以避免遇到自动插入分号陷阱
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const name = '大姐姐'
-const element = <h1>hello, {name}</h1>
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName
+}
+
+const user = {
+  firstName: '大',
+  lastName: '姐姐1'
+}
+
+const element = (
+  <h1>
+    hello, {formatName(user)}
+  </h1>
+)
 
 ReactDOM.render(
   element,
