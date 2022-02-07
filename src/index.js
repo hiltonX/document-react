d382f3d58a9da506ffb5cee069660734068ce038
大姐姐 <wzxielulu@163.com>
2022-02-07 16:15:39 +0800

    【4.组件&Props】提取组件

--- src/index.js
+++ src/index.js
@@ -44,34 +44,88 @@ import ReactDOM from 'react-dom'
 // }
 
 // class定义
-class Welcome extends React.Component {
-  render() {
-    return <h1>hello, {this.props.name}</h1>
-  }
-}
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>hello, {this.props.name}</h1>
+//   }
+// }
 
 // const element = <Welcome name="大姐姐1"/>
 
-function App() {
-  return (
-    <div>
-      <Welcome name="大姐姐" />
-      <Welcome name="大姐姐1" />
-      <Welcome name="大姐姐2" />
-    </div>
-  )
-}
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="大姐姐" />
+//       <Welcome name="大姐姐1" />
+//       <Welcome name="大姐姐2" />
+//     </div>
+//   )
+// }
 
 // 过程
 // 调用ReactDOM.render()函数，传入<Welcome name="大姐姐1" />作为参数
 // React调用Welcome组件，将 {name: '大姐姐1'}作为props传入
 // Welcome组件将<h1>Hello, 大姐姐1</h1>元素作为返回值
 // React DOM将DOM高效地更新为<h1>Hello, 大姐姐1</h1>
+
+
+// 提取组件
+function formatDate(text) {
+  return text
+}
+// 建议从组件自身角度命名props
+function Avatar(props) {
+  return (<img 
+    src={props.user.avatarUrl}
+    alt={props.user.name}
+  />)
+}
+
+function UserInfo(props) {
+  return (
+    <div className="user-info">
+      <Avatar user={props.user} />
+      <div className="user-info-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
+
+function Comment(props) {
+  return (<div className="comment">
+    {/* <div className="user-info">
+      <img className="avatar" 
+        src={props.author.avatarUrl}
+        alt={props.author.name}
+      />
+      <div className="user-info-name">
+        {props.author.name}
+      </div>
+    </div> */}
+    <UserInfo user={props.author}/>
+    <div className="comment-text">
+      {props.text}
+    </div>
+    <div className="comment-date"> 
+      {formatDate(props.date)}
+    </div>
+  </div>)
+}
+
+
 ReactDOM.render(
   // Welcome({
   //   name: '大姐姐'
   // }),
   // element,
-  App(),
+  Comment({
+    author: {
+      name: '李健',
+      avatarUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%3A%2F%2Fdingyue.ws.126.net%2F2021%2F0306%2F701a4922j00qpjng1001nc000ht00dom.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1646308771&t=376abf8646bcad35c9394b6b82c363b4'
+    },
+    text: '李健呜呜呜呜',
+    date: '2020-01-01'
+  }),
   document.getElementById('root')
 )
