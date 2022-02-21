5a97361e44caa8be04312004359f1cb6d135d290
大姐姐 <WZxielulu@163.com>
2022-02-21 11:02:16 +0800

    【1.无障碍】标准和指南

--- src/advanced/index.js
+++ src/advanced/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
+// ========= 无障碍 =========
+// 标准和指南
+function Input() {
+  return (<input 
+    type="text"
+    aria-label="name"
+    aria-requied={true}
+    onChange={() => console.log('onChange')}
+    placeholder="aria-*"
+    name="name"
+  />)
+}
+
 export default class Advanced extends React.Component {
 
   render() {
@@ -7,6 +20,8 @@ export default class Advanced extends React.Component {
       <div className="title">无障碍</div>
       <div className="sub-title">无障碍辅助功能</div>
       <div className="des">无障碍辅助功能是使得辅助技术正确解读网页的必要条件</div>
+      <div className="sub-title">标准和指南</div>
+      <Input />
     </div>)
   }
 }
