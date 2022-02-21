df7f8c7c63eed870ce844a4a09e4f45ec6d89079
大姐姐 <WZxielulu@163.com>
2022-02-21 10:48:51 +0800

    【1.无障碍】为什么使用无障碍

--- src/advanced/index.js
+++ src/advanced/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default class Advanced extends React.Component {
 
   render() {
-    return (<div>advanced</div>)
+    return (<div>
+      <div className="title">无障碍</div>
+      <div className="sub-title">无障碍辅助功能</div>
+      <div className="des">无障碍辅助功能是使得辅助技术正确解读网页的必要条件</div>
+    </div>)
   }
 }
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      key: 'concept'
+      key: 'advanced'
     }
   }
 
